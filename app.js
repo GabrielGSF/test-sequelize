52430ef283be411e6b65e862f82a0158c477886d
Gabriel Santos <gabrielsantos.gsf@gmail.com>
2022-03-22 21:27:10 -0300

    Sequelize modelo básico

---
+++ app.js
@@ -0,0 +1,15 @@
+(async () => {
+
+    const database = require('./db')
+    const Produto = require('./produto')
+    try {
+        const resultado = 
+        console.log('Connection has been established successfully')
+    } catch(error) {
+        console.error('Unable to connect to the database:', error)
+    }
+})();
+    
+    
+
+   
